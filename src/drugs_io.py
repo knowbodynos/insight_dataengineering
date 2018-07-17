0563a699181772fba55483a1aaf0d3292c5e7339
Ross Altman <knowbodynos@gmail.com>
2018-07-17 14:39:26 -0400

    Finished.

--- src/drugs_io.py
+++ src/drugs_io.py
@@ -23,11 +23,7 @@ def write_drug_table_to_file(drug_table, file_name):
     """Write table (i.e. list of lists) given by [[drug_name, num_prescriber, total_cost], ...]
     to file, including appropriate header line."""
     with open(file_name, "w") as out_stream:
-        print("drug_name,num_prescriber,total_cost", file = out_stream)
-        for i in range(len(drug_table) - 1):
-            drug_name, num_prescriber, total_cost = drug_table[i]
+        out_stream.write("drug_name,num_prescriber,total_cost")
+        for drug_name, num_prescriber, total_cost in drug_table:
             total_cost = cost_to_string(total_cost)
-            print("{0},{1},{2}".format(drug_name, num_prescriber, total_cost), file = out_stream)
-        drug_name, num_prescriber, total_cost = drug_table[-1]
-        total_cost = cost_to_string(total_cost)
-        print("{0},{1},{2}".format(drug_name, num_prescriber, total_cost), file = out_stream, end = '')
+            out_stream.write("\n{0},{1},{2}".format(drug_name, num_prescriber, total_cost))
